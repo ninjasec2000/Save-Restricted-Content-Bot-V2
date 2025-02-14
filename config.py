703ba5011a4de492eadb3200d2974afe5d8f47a3
ninjasec2000 <ninjasec2000@gmail.com>
2025-02-14 18:54:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,28 +3,17 @@
 
 from os import getenv
 
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
-
-INST_COOKIES = """
-# wtite up here insta cookies
-"""
-
-YTUB_COOKIES = """
-# write here yt cookies
-"""
-
 API_ID = int(getenv("API_ID", "21268963"))
 API_HASH = getenv("API_HASH", "60239424717807c61aa7e446d443984f")
-BOT_TOKEN = getenv("BOT_TOKEN", "7874751517:AAGk5u7lGbdq9SfEE_HF0FOwc83MZS-N8xQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "7483825602:AAHkUSZmC_qlJzR62MnOhO5udo9EPdU2-fg")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5955963998").split()))
 MONGO_DB = getenv("MONGO_DB", None)
 LOG_GROUP = getenv("LOG_GROUP", "-1002363247735")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002438586582"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "10"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+YT_COOKIES = getenv("YT_COOKIES", None)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
